8349f4cd273f7bc65525486de7c5ad6e3764314a
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-05-04 14:26:18 +0530

    wicket counter working

--- app/src/main/java/com/example/jaspreetsingh/cricketscore/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/cricketscore/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int score = 0;
+    int wicket = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
         score = score + 6;
         displayScore(score);
     }
+    public void wicket (View view)   {
+        wicket = wicket + 1;
+        dispalyWicket(wicket);
+    }
 
     public void displayScore(int score)  {
         TextView totalScore = (TextView) findViewById(R.id.run_text_view);
         totalScore.setText(String.valueOf(score));
+    }
+    public void dispalyWicket(int wicket)   {
+        TextView totalWicket = (TextView) findViewById(R.id.wicket_text_view);
+        totalWicket.setText(String.valueOf(wicket));
     }
 }
