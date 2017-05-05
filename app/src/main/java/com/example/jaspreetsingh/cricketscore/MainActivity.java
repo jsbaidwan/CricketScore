9d36638a16bd72b3c9b3fba0f98a8e48823a9b62
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-05-06 00:50:47 +0530

    add toast that display ALL OUT on 10th wicket

--- app/src/main/java/com/example/jaspreetsingh/cricketscore/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/cricketscore/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int score = 0;
@@ -13,10 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void wicket (View view)   {
         wicket = wicket + 1;
         if (wicket<11) {
             displayWicket(wicket);
+        }
+        if(wicket==10){
+            Toast.makeText(getApplicationContext(),"ALL OUT",Toast.LENGTH_SHORT).show();
         }
     }
     public void oneRun(View v)  {
